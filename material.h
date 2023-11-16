f96af9c326f900743f690b449168be2121c0a2d1
Sebastián Solorzano <sol21826@uvg.edu.gt>
2023-11-16 15:03:28 -0600

    Clases

---
+++ material.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "color.h"
+
+struct Material {
+  Color diffuse;
+  float albedo;
+  float specularAlbedo;
+  float specularCoefficient;
+  float reflectivity;
+  float transparency;
+  float refractionIndex;
+};
